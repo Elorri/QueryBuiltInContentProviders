de335228a77b228afcd82dab62e29dba985c0bed
Elorri <etchemendy.elorri@gmail.com>
2017-09-09 16:54:23 +0200

    Browsed android providers and listed corresponding uris

--- app/src/main/java/com/elorri/android/querybuiltincontentproviders/MainActivity.java
+++ app/src/main/java/com/elorri/android/querybuiltincontentproviders/MainActivity.java
@@ -1,6 +1,16 @@
 package com.elorri.android.querybuiltincontentproviders;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Browser;
+import android.provider.CalendarContract;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.provider.Telephony;
+import android.provider.UserDictionary;
+import android.provider.VoicemailContract;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.BufferedWriter;
@@ -21,6 +31,138 @@ public class MainActivity extends AppCompatActivity {
         // not expandable in size. I will write to external storage to be sure I can access the
         // file from my phone and send it on drop box or via email.
         File providersOverview = getFileToWriteIn();
+
+        //List of android builtin content providers static uris that can be queried.
+        //Dynamics uris have been listed here and therefore some provider don't show up (eg
+        // DocumentProvider because it uses only dynamic uris).
+        Uri[] builtInUris = new Uri[]{
+                Browser.BOOKMARKS_URI,
+                Browser.SEARCHES_URI,
+                CalendarContract.CONTENT_URI,
+                CalendarContract.CalendarEntity.CONTENT_URI,
+                CalendarContract.Calendars.CONTENT_URI,
+                CalendarContract.Attendees.CONTENT_URI,
+                CalendarContract.EventsEntity.CONTENT_URI,
+                CalendarContract.Events.CONTENT_URI,
+                CalendarContract.Events.CONTENT_EXCEPTION_URI,
+                CalendarContract.Instances.CONTENT_URI,
+                CalendarContract.Instances.CONTENT_BY_DAY_URI,
+                CalendarContract.Instances.CONTENT_SEARCH_URI,
+                CalendarContract.Instances.CONTENT_SEARCH_BY_DAY_URI,
+                CalendarContract.CalendarCache.URI,
+                CalendarContract.EventDays.CONTENT_URI,
+                CalendarContract.Reminders.CONTENT_URI,
+                CalendarContract.CalendarAlerts.CONTENT_URI,
+                CalendarContract.CalendarAlerts.CONTENT_URI_BY_INSTANCE,
+                CalendarContract.Colors.CONTENT_URI,
+                CalendarContract.ExtendedProperties.CONTENT_URI,
+                CalendarContract.SyncState.CONTENT_URI,
+                CallLog.CONTENT_URI,
+                CallLog.Calls.CONTENT_URI,
+                CallLog.Calls.CONTENT_FILTER_URI,
+                CallLog.Calls.CONTENT_URI_WITH_VOICEMAIL,
+                ContactsContract.AUTHORITY_URI,
+                ContactsContract.Directory.CONTENT_URI,
+                ContactsContract.SyncState.CONTENT_URI,
+                ContactsContract.ProfileSyncState.CONTENT_URI,
+                ContactsContract.Contacts.CONTENT_URI,
+                ContactsContract.Contacts.CORP_CONTENT_URI,
+                ContactsContract.Contacts.CONTENT_LOOKUP_URI,
+                ContactsContract.Contacts.CONTENT_VCARD_URI,
+                ContactsContract.Contacts.CONTENT_MULTI_VCARD_URI,
+                ContactsContract.Contacts.CONTENT_FILTER_URI,
+                ContactsContract.Contacts.CONTENT_STREQUENT_URI,
+                ContactsContract.Contacts.CONTENT_FREQUENT_URI,
+                ContactsContract.Contacts.CONTENT_STREQUENT_FILTER_URI,
+                ContactsContract.Contacts.CONTENT_GROUP_URI,
+                ContactsContract.Profile.CONTENT_URI,
+                ContactsContract.Profile.CONTENT_VCARD_URI,
+                ContactsContract.Profile.CONTENT_RAW_CONTACTS_URI,
+                ContactsContract.DeletedContacts.CONTENT_URI,
+                ContactsContract.RawContacts.CONTENT_URI,
+                ContactsContract.Data.CONTENT_URI,
+                ContactsContract.RawContactsEntity.CONTENT_URI,
+                ContactsContract.RawContactsEntity.PROFILE_CONTENT_URI,
+                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                ContactsContract.StatusUpdates.CONTENT_URI,
+                ContactsContract.StatusUpdates.PROFILE_CONTENT_URI,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI,
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Email.CONTENT_LOOKUP_URI,
+                ContactsContract.CommonDataKinds.Email.CONTENT_FILTER_URI,
+                ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Callable.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Callable.CONTENT_FILTER_URI,
+                ContactsContract.CommonDataKinds.Contactables.CONTENT_URI,
+                ContactsContract.CommonDataKinds.Contactables.CONTENT_FILTER_URI,
+                ContactsContract.Groups.CONTENT_URI,
+                ContactsContract.Groups.CONTENT_SUMMARY_URI,
+                ContactsContract.AggregationExceptions.CONTENT_URI,
+                ContactsContract.Settings.CONTENT_URI,
+                ContactsContract.ProviderStatus.CONTENT_URI,
+                ContactsContract.DataUsageFeedback.FEEDBACK_URI,
+                ContactsContract.DataUsageFeedback.DELETE_USAGE_URI,
+                ContactsContract.DisplayPhoto.CONTENT_URI,
+                ContactsContract.DisplayPhoto.CONTENT_MAX_DIMENSIONS_URI,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI,
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                MediaStore.Audio.Media.INTERNAL_CONTENT_URI,
+                MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
+                MediaStore.Audio.Genres.INTERNAL_CONTENT_URI,
+                MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
+                MediaStore.Audio.Playlists.INTERNAL_CONTENT_URI,
+                MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI,
+                MediaStore.Audio.Artists.INTERNAL_CONTENT_URI,
+                MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+                MediaStore.Audio.Albums.INTERNAL_CONTENT_URI,
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                MediaStore.Video.Media.INTERNAL_CONTENT_URI,
+                MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI,
+                MediaStore.Video.Thumbnails.INTERNAL_CONTENT_URI,
+                Settings.System.CONTENT_URI,
+                Settings.System.DEFAULT_ALARM_ALERT_URI,
+                Settings.System.DEFAULT_NOTIFICATION_URI,
+                Settings.System.DEFAULT_RINGTONE_URI,
+                Settings.Secure.CONTENT_URI,
+                Settings.Global.CONTENT_URI,
+                Settings.Bookmarks.CONTENT_URI,
+                Telephony.Sms.CONTENT_URI,
+                Telephony.Sms.Inbox.CONTENT_URI,
+                Telephony.Sms.Sent.CONTENT_URI,
+                Telephony.Sms.Draft.CONTENT_URI,
+                Telephony.Sms.Outbox.CONTENT_URI,
+                Telephony.Sms.Conversations.CONTENT_URI,
+                Telephony.Threads.CONTENT_URI,
+                Telephony.Threads.OBSOLETE_THREADS_URI,
+                Telephony.Mms.CONTENT_URI,
+                Telephony.Mms.REPORT_REQUEST_URI,
+                Telephony.Mms.REPORT_STATUS_URI,
+                Telephony.Mms.Inbox.CONTENT_URI,
+                Telephony.Mms.Sent.CONTENT_URI,
+                Telephony.Mms.Draft.CONTENT_URI,
+                Telephony.Mms.Outbox.CONTENT_URI,
+                Telephony.Mms.Rate.CONTENT_URI,
+                Telephony.MmsSms.CONTENT_URI,
+                Telephony.MmsSms.CONTENT_CONVERSATIONS_URI,
+                Telephony.MmsSms.CONTENT_FILTER_BYPHONE_URI,
+                Telephony.MmsSms.CONTENT_UNDELIVERED_URI,
+                Telephony.MmsSms.CONTENT_DRAFT_URI,
+                Telephony.MmsSms.CONTENT_LOCKED_URI,
+                Telephony.MmsSms.SEARCH_URI,
+                Telephony.MmsSms.PendingMessages.CONTENT_URI,
+                Telephony.Carriers.CONTENT_URI,
+                Telephony.CellBroadcasts.CONTENT_URI,
+                UserDictionary.CONTENT_URI,
+                UserDictionary.Words.CONTENT_URI,
+                VoicemailContract.Voicemails.CONTENT_URI,
+                VoicemailContract.Status.CONTENT_URI
+        };
+
+
         writeTableOfStringToFile(providersOverview, new String[]{"Hello", "World"});
     }
 
